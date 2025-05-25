dea768ff73b85bdf5fcc2e492740b2599dc6afbc
eaguadov <eaguadov@gmail.com>
2025-05-25 19:55:19 +0200

    Add files via upload

---
+++ fichar-entrada.js
@@ -0,0 +1,26 @@
+
+const { chromium } = require('playwright');
+require('dotenv').config();
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('https://easyclocking.net/?ReturnUrl=%2femployee%2ftimecard');
+
+  await page.fill('input[name="CompanyID"]', process.env.COMPANY_ID);
+  await page.fill('input[name="UserName"]', process.env.USER_NAME);
+  await page.fill('input[name="Password"]', process.env.PASSWORD);
+  await page.click('button[type="submit"]');
+
+  await page.waitForTimeout(5000);
+  const clockInButton = await page.$('text="Clock In"');
+  if (clockInButton) {
+    await clockInButton.click();
+    console.log("Fichaje de entrada realizado correctamente.");
+  } else {
+    console.log("No se encontró el botón Clock In.");
+  }
+
+  await browser.close();
+})();
---
+++ fichar-salida.js
@@ -0,0 +1,26 @@
+
+const { chromium } = require('playwright');
+require('dotenv').config();
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('https://easyclocking.net/?ReturnUrl=%2femployee%2ftimecard');
+
+  await page.fill('input[name="CompanyID"]', process.env.COMPANY_ID);
+  await page.fill('input[name="UserName"]', process.env.USER_NAME);
+  await page.fill('input[name="Password"]', process.env.PASSWORD);
+  await page.click('button[type="submit"]');
+
+  await page.waitForTimeout(5000);
+  const clockOutButton = await page.$('text="Clock Out"');
+  if (clockOutButton) {
+    await clockOutButton.click();
+    console.log("Fichaje de salida realizado correctamente.");
+  } else {
+    console.log("No se encontró el botón Clock Out.");
+  }
+
+  await browser.close();
+})();
